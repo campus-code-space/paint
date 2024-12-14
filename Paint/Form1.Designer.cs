43e85dda7e9c944607bf21ad7d8197bf0db880fa
Leul Zewdu <Leulzewduchanie@gmail.com>
2024-12-14 08:42:21 +0300

    added colors

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -29,16 +29,148 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 450);
             panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Controls.Add(pictureBox10);
+            panel2.Controls.Add(pictureBox9);
+            panel2.Controls.Add(pictureBox8);
+            panel2.Controls.Add(pictureBox7);
+            panel2.Controls.Add(pictureBox5);
+            panel2.Controls.Add(pictureBox6);
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(489, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(299, 103);
+            panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Red;
+            pictureBox1.Location = new Point(3, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(34, 31);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Blue;
+            pictureBox2.Location = new Point(58, 13);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(38, 31);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Yellow;
+            pictureBox3.Location = new Point(117, 13);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(35, 31);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Silver;
+            pictureBox4.Location = new Point(170, 13);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(40, 31);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Green;
+            pictureBox6.Location = new Point(234, 13);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(40, 31);
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Fuchsia;
+            pictureBox5.Location = new Point(3, 60);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(40, 31);
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(64, 0, 64);
+            pictureBox7.Location = new Point(58, 60);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(40, 31);
+            pictureBox7.TabIndex = 7;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.White;
+            pictureBox8.Location = new Point(117, 60);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(40, 31);
+            pictureBox8.TabIndex = 8;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox9.Location = new Point(170, 60);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(40, 31);
+            pictureBox9.TabIndex = 9;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackColor = Color.FromArgb(255, 192, 192);
+            pictureBox10.Location = new Point(234, 60);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(40, 31);
+            pictureBox10.TabIndex = 10;
+            pictureBox10.TabStop = false;
             // 
             // Form1
             // 
@@ -48,11 +180,34 @@
             Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
     }
 }
